a5ace0c6bcbf5015be0405dc1885b9c4c79e0ea7
Patryk Mysiorski <mysior@ncdc.pl>
2020-11-10 17:14:09 +0100

    move navbar logic to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 import "./App.scss";
-import NavBar from "../NavBar/NavBar";
+import logo from "../../logo.svg";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import News from "../News/News";
+import GalleriesContainer from "../GalleriesContainer/GalleriesContainer";
+import About from "../About/About";
 
 const App: React.FC = () => {
   return (
     <div className={"app-container"}>
-      <NavBar />
+      <Router>
+        <header>
+          <div className={"header"}>
+            <img className={"logo"} src={logo} alt="app logo" height={70} />
+            <nav>
+              <ul className={"nav__links"}>
+                <li>
+                  <Link to="/">Galleries</Link>
+                </li>
+                <li>
+                  <Link to="/news">News</Link>
+                </li>
+              </ul>
+            </nav>
+            <Link to="/contact">
+              <button type="button" className={"contact-button"}>
+                Contact us
+              </button>
+            </Link>
+          </div>
+        </header>
+        <div className="app-content">
+          <Switch>
+            <Route path="/news">
+              <News />
+            </Route>
+            <Route exact path="/">
+              <GalleriesContainer />
+            </Route>
+            <Route path="/contact">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
