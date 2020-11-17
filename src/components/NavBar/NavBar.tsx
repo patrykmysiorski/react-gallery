7b9728e8d24d2faa2de7c14d715d5d482f8ee001
Patryk Mysiorski <mysior@ncdc.pl>
2020-11-18 00:12:12 +0100

    add simple user options in header

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import logo from "../../logo.svg";
 import { Link } from "react-router-dom";
 import { NEWS_URL } from "../../constants/urls";
 import "./NavBar.scss";
+import UserPanel from "../UserPanel/UserPanel";
 
-const NavBar = () => (
-  <header>
-    <div className={"header"}>
-      <div
-        className={classNames(
-          "logo-part",
-          "flex-row-container",
-          "flex-align-items-center"
-        )}
-      >
-        <img className={"logo"} src={logo} alt="app logo" height={70} />
-        <h2>React Gallery</h2>
+const NavBar = () => {
+  const avatarUrl = "/img/user/user.jpeg";
+  const [userPanelhidden, setUserPanelHidden] = useState(true);
+  return (
+    <header>
+      <div className={"header"}>
+        <Link to={"/"}>
+          <div
+            className={classNames(
+              "logo-part",
+              "flex-row-container",
+              "flex-align-items-center"
+            )}
+          >
+            <img className={"logo"} src={logo} alt="app logo" height={70} />
+            <h2>React Gallery</h2>
+          </div>
+        </Link>
+        <div className="links-part flex-row-container">
+          <div className="buttons flex-row-container flex-justify-end">
+            <Link to={"/"}>Galleries</Link>
+            <Link to={`${NEWS_URL}`}>News</Link>
+          </div>
+          <div
+            className="user-panel-header m-r-1 m-l-1"
+            onClick={() => setUserPanelHidden(!userPanelhidden)}
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL + avatarUrl})`,
+            }}
+          />
+        </div>
       </div>
-      <div className="buttons flex-row-container flex-justify-end">
-        <Link to={"/"}>Galleries</Link>
-        <Link to={`${NEWS_URL}`}>News</Link>
-      </div>
-      {/*<Link to={"/"}>*/}
-      {/*  <button>Galleries</button>*/}
-      {/*</Link>*/}
-      {/*<Link to={`${CONTACT_URL}`}>*/}
-      {/*  <button type="button" className={"contact-button"}>*/}
-      {/*    Contact us*/}
-      {/*  </button>*/}
-      {/*</Link>*/}
-    </div>
-  </header>
-);
+      {!userPanelhidden && <UserPanel />}
+    </header>
+  );
+};
 
 export default NavBar;
---
+++ src/components/UserPanel/UserPanel.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UserPanel.scss";
+
+const UserPanel = () => (
+  <div className="user-panel p-2">user options in future</div>
+);
+
+export default UserPanel;
